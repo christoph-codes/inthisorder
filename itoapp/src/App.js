b9122501e6e6cd575ba0e461430bc79784aec628
tkcwebdev <tkcwebdev@gmail.com>
2019-12-30 15:39:56 -0800

    deleted auth wrapper attempt

--- itoapp/src/App.js
+++ itoapp/src/App.js
@@ -2,46 +2,22 @@ import React from "react";
 import "./App.css";
 import HeaderNav from "./marketing/components/navigation/HeaderNav/HeaderNav";
 
-import { BrowserRouter as Router } from "react-router-dom";
-import AuthRoute from './components/AuthRoute';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import MarketingMain from "./marketing/components/MarketingMain";
 import AdminMain from "./admin/components/AdminMain";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null,
-      authenticated: false
-    }
-    this.setCurrentUser = this.setCurrentUser.bind(this);
-  }
-
-  setCurrentUser(user) {
-    if(user) {
-      this.setState({
-        currentUser: user,
-        authenticated: true
-      })
-    } else {
-      this.setState({
-        currentUser: null,
-        authenticated: false
-      })
-    }
-  }
   render() {
     return (
         <Router>
           <div className="App">
             <HeaderNav />
             <MarketingMain />
-            <AuthRoute 
+            <Route 
             exact
             component={AdminMain}
-            authenticated={this.state.authenticated}
-            path="/admin"
+            path="/admin/dashboard"
              />
           </div>
         </Router>
