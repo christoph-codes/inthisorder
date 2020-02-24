e9dcbef6cd802757501b6c6ee567f88232a493c4
Christopher Jones <tkcwebdev@gmail.com>
2020-02-24 13:55:38 -0800

    Updated Admin Navigation Initiation

--- itoapp/src/components/adminNavigation/AdminNavigation.component.js
+++ itoapp/src/components/adminNavigation/AdminNavigation.component.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useHistory, Link } from "react-router-dom";
+import firebase from 'firebase';
+import './AdminNavigation.scss';
 
 export default function AdminNavigation() {
+    const history = useHistory();
+
+    const signOut = () => {
+        firebase.auth().signOut().then(() => {
+          history.push('/login');
+        }).catch((error) => {
+          // An error happened.
+        });
+      }
     return (
         <div className="AdminNavigation">
-            <p>Navigation</p>
+            <ul>
+                <li><Link to="/admin/dashboard">Dashboard</Link></li>
+                <li><Link to="/admin/edit-tasks">Edit Tasks</Link></li>
+                <li><Link to="/admin/settings">Settings</Link></li>
+                <li><button className="link" onClick={signOut}>Logout</button></li>
+            </ul>
         </div>
     )
 }
--- itoapp/src/components/adminNavigation/AdminNavigation.container.js
+++ itoapp/src/components/adminNavigation/AdminNavigation.container.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import './AdminNavigation.scss';
-import AdminNavigation from './AdminNavigation.component';
+import React from "react";
+import "./AdminNavigation.scss";
+import AdminNavigation from "./AdminNavigation.component";
 
 export default function AdminNavigationContainer() {
-    return (
-        <div className="AdminNavigationContainer">
-            <AdminNavigation />
-        </div>
-    )
+  return (
+    <div className="AdminNavigationContainer">
+      <div className="uk-container">
+        <AdminNavigation />
+      </div>
+    </div>
+  );
 }
