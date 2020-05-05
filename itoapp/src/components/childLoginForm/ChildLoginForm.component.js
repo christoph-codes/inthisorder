925ecda2f61ea632ad7d5379ad58e1ed426397c6
tkcwebdev <tkcwebdev@gmail.com>
2020-05-05 12:16:22 -0700

    Working on Child Login Function to check db for parent WIP

--- itoapp/src/components/childLoginForm/ChildLoginForm.component.js
+++ itoapp/src/components/childLoginForm/ChildLoginForm.component.js
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import {useHistory} from 'react-router-dom';
+import db from '../../config/firebaseConfig';
 
 export default function ChildLoginForm(props) {
   const [familyCode, setFamilyCode] = useState("");
   const [childName, setChildName] = useState("");
   const [childPin, setChildPin] = useState("");
   const [feedback, setFeedback] = useState("");
+  const [children, setChildren] = useState([]);
   const history = useHistory();
 
   const validatePin = (e) => {
@@ -21,7 +23,31 @@ export default function ChildLoginForm(props) {
 
   const login = (e) => {
     e.preventDefault();
-    console.log(familyCode, childName, childPin);
+    // WIP TODO NEXT!
+    let users = db.collection('users').where('familycode', '==', familyCode);
+    users.get()
+    .then(snapshot => {
+      if(snapshot.empty) {
+        setFeedback('Family Code is incorrect');
+      } else {
+        snapshot.docs.map(doc => {
+          let parent = doc.data();
+          console.log(parent)
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err.message);
+    })
+    // console.log(users.get())
+    // users.get().then(snapshot => {
+    //   snapshot.docs.map(doc => {
+    //     console.log(doc.exists)
+    //   })
+    // }).catch(err => {
+    //   console.log(err);
+    // })
+    // console.log(familyCode, childName, childPin);
   };
   return (
     <div className="ChildLoginForm">
@@ -32,13 +58,11 @@ export default function ChildLoginForm(props) {
           type="text"
           placeholder="Family Code"
         />
+        <select value={childName} className="uk-select uk-margin-small" disabled={!familyCode} onChange={(e) => setChildName(e.target.value)}>
+          <option value='' disabled>What is your name</option>
+        </select>
         <input
-          className="uk-input uk-margin-small"
-          onChange={(e) => setChildName(e.target.value)}
-          type="text"
-          placeholder="Name"
-        />
-        <input
+         disabled={!familyCode}
           className="uk-input"
           placeholder="4 Digit Pin"
           type="text"
