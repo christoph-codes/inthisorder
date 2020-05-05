fd7cc1e2236f60753671014739e7b8411bee852c
tkcwebdev <tkcwebdev@gmail.com>
2020-05-05 08:25:58 -0700

    Fix auth redirect when logging out to set statuses to null

--- itoapp/src/components/auth/PrivateRoute.js
+++ itoapp/src/components/auth/PrivateRoute.js
@@ -6,11 +6,7 @@ import Spinner from "../../ui/spinner/Spinner";
 export default function PrivateRoute({ component: RouteComponent, ...rest }) {
   const { currentUser, userData } = useContext(AuthContext);
 
-<<<<<<< HEAD
     if(currentUser === false) {
-=======
-    if(isLoggedIn === false) {
->>>>>>> ce2f548d37206505ee18caeb5b69d977af01ae2c
       return <Redirect to="/login" />
     }
     return (
