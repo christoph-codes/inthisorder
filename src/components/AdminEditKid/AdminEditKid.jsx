29ab935aae053fa6aaf067771d055d72a9eca503
Christopher Jones <tkcwebdev@gmail.com>
2021-09-18 17:21:30 -0700

    refactor: Complete refactor to use reusable components and added functions to properly make updates to a child.

--- src/components/AdminEditKid/AdminEditKid.jsx
+++ src/components/AdminEditKid/AdminEditKid.jsx
@@ -1,42 +1,45 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
-import UIkit from 'uikit';
-import db from '../../config/firebaseConfig';
-import './AdminEditKid.scss';
-import { UserContext } from '../../providers/UserProvider';
+import React, { useState, useContext } from 'react';
+import { useParams, useHistory, Redirect } from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
+import Button from '../Button';
+import Input from '../Input';
 import { convertTimestamp } from '../../util/helper';
+import { KidsContext } from '../../providers/KidsProvider';
+import CenteredDashContainer from '../CenteredDashContainer';
+import './AdminEditKid.scss';
 
 const AdminEditKid = () => {
 	const { slug } = useParams();
 	const history = useHistory();
-	const { user } = useContext(UserContext);
-	const [child, setChild] = useState({
-		name: '',
-		age: '',
-		pin: '',
+	const { kids, areKidsLoading, updateChild } = useContext(KidsContext);
+	const [child, setChild] = useState(() => {
+		if (history?.location?.state?.child) {
+			console.log('history object?');
+			return history.location.state.child;
+		}
+
+		console.log('kids:', kids);
+		if (!areKidsLoading) {
+			return kids.find((singleChild) => {
+				console.log('single child', singleChild.name);
+				if (singleChild.name === slug) {
+					return singleChild;
+				}
+				return null;
+			});
+		}
+		return {
+			name: '',
+			age: '',
+			pin: '',
+		};
 	});
 	const [feedback, setFeedback] = useState(null);
 
-	useEffect(() => {
-		// Get selected kid from url slug
-		const kid = db
-			.collection('users')
-			.doc(user.email)
-			.collection('kids')
-			.where('name', '==', slug);
-
-		const unsubscribe = kid.get().then((snapshot) => {
-			snapshot.forEach((doc) => {
-				const dbChild = doc.data();
-				dbChild.id = doc.id;
-				setChild(dbChild);
-			});
-		});
-		return () => unsubscribe;
-	}, [user, slug]);
-
 	const updateField = (e) => {
-		setChild({ ...child, [e.target.name]: e.target.value });
+		setChild((prev) => {
+			return { ...prev, [e.target.name]: e.target.value };
+		});
 	};
 
 	const validatePin = (e) => {
@@ -46,143 +49,88 @@ const AdminEditKid = () => {
 		} else {
 			setFeedback('You must enter a valid 4 digit pin number.');
 		}
+		updateField(e);
 	};
 
-	const updateChild = (e) => {
+	const submitChildUpdate = (e) => {
 		e.preventDefault();
-		if (feedback === null || feedback === '') {
-			const kid = db
-				.collection('users')
-				.doc(user.email)
-				.collection('kids')
-				.doc(child.id);
-			kid.update({
-				name: child.name,
-				age: child.age,
-				pin: child.pin,
+		if (child.name && child.age && child.pin && feedback) {
+			updateChild(child.id, {
+				...child,
 				last_updated: new Date(),
-			})
-				.then(() => {
-					history.push('/admin/kids');
-					UIkit.notification(
-						"<span uk-icon='icon: check'></span> Child Successfully Updated.",
-						{ pos: 'bottom-right' }
-					);
-				})
-				.catch((err) => {
-					setFeedback(err.message);
-				});
+			});
 		} else {
 			setFeedback('One or more of your fields were not completed');
 		}
 	};
 
-	const deleteChild = (id) => {
-		const task = db
-			.collection('users')
-			.doc(user.email)
-			.collection('kids')
-			.doc(id);
-		task.delete()
-			.then(() => {
-				history.push('/admin/kids');
-				UIkit.notification(
-					"<span uk-icon='icon: check'></span> Child Deleted!",
-					{ pos: 'bottom-right' }
-				);
-			})
-			.catch((error) => {
-				console.error('Error removing document: ', error);
-			});
-	};
+	if (!child.name && !child.pin && !child.age) {
+		return <Redirect to="/admin/kids" />;
+	}
 
 	return (
-		<div className="AdminEditKid">
-			<div className="main">
-				<h1 className="uk-text-center">
-					Edit Child
-					{slug}
-				</h1>
-				{child.last_updated ? (
-					<p className="last_update uk-text-center">
-						Last Update: {convertTimestamp(child.last_updated)}
-					</p>
-				) : null}
-				<form onSubmit={updateChild}>
-					<label
-						htmlFor="name"
-						className="uk-form-controls uk-form-label"
-					>
-						Child Name
-						<input
-							className="uk-input uk-margin"
-							placeholder="Name of Child"
-							value={child.name}
-							type="text"
-							name="name"
-							onChange={updateField}
-						/>
-					</label>
-					<label
-						htmlFor="age"
-						className="uk-form-controls uk-form-label"
-					>
-						Child Age
-						<input
-							className="uk-input"
-							placeholder="Age of the Child"
-							type="number"
-							name="age"
+		<CenteredDashContainer className="AdminEditKid">
+			<h1>{child.name}</h1>
+			{child.createdon || child.last_updated ? (
+				<p className="last_update mb-3">
+					Last Update:{' '}
+					{convertTimestamp(child.last_updated || child.createdon)}
+				</p>
+			) : null}
+			<p className="text-primary-light fw-bold">
+				<small>
+					Be sure to help your child remember their 4 digit pin to
+					login!
+				</small>
+			</p>
+			<form onSubmit={submitChildUpdate}>
+				<Input
+					label="Name of the child"
+					placeholder="Kennedy"
+					value={child.name}
+					type="text"
+					name="name"
+					onChange={updateField}
+				/>
+
+				<Row>
+					<Col>
+						<Input
+							inputClass="border border-2 border-transparent"
+							label="Child Age"
+							placeholder="8"
 							value={child.age}
+							type="text"
+							name="age"
 							onChange={updateField}
 						/>
-					</label>
-					<p className="uk-form-label">
-						Child Pin{' '}
-						<span className="uk-float-right">
-							<small>
-								Be sure to help your child remember their 4
-								digit pin to login!
-							</small>
-						</span>
-					</p>
-					<label htmlFor="pin" className="uk-form-controls">
-						<input
-							className="uk-input"
+					</Col>
+					<Col>
+						<Input
+							inputClass="border border-2 border-primary-light"
+							label="Child Pin"
+							value={child.pin}
 							placeholder="4 Digit Pin"
 							type="text"
-							name="pin"
+							name="name"
 							pattern="^[0-9]*$"
-							onChange={updateField}
-							onKeyUp={validatePin}
-							value={child.pin}
+							onChange={validatePin}
 							maxLength="4"
 							required
 						/>
-					</label>
-					<p className="feedback">{feedback}</p>
-					<input
-						type="submit"
-						className="uk-button uk-button-default uk-button-primary"
-						value="Submit"
-					/>
-					<button
-						className="uk-button uk-button-default next-btn"
-						onClick={() => history.push('/admin/kids')}
-						type="button"
-					>
-						Cancel
-					</button>
-					<button
-						className="uk-button uk-button-default next-btn danger"
-						onClick={() => deleteChild(child.id)}
-						type="button"
-					>
-						Delete Child
-					</button>
-				</form>
-			</div>
-		</div>
+					</Col>
+				</Row>
+
+				<p className="text-secondary">{feedback}</p>
+				<Button type="submit">Update Child</Button>
+				<Button
+					variant="inactive-ghosted"
+					onClick={() => history.push('/admin/kids')}
+				>
+					Cancel
+				</Button>
+			</form>
+		</CenteredDashContainer>
 	);
 };
 
