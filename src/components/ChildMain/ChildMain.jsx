a8edab643c4004b34f39501c9b31282e9d61f812
Christopher Jones <christopher.jones@creditone.com>
2021-09-21 00:38:04 -0700

    refactor: Removing revolving spinner attempt for child dashboard.

--- src/components/ChildMain/ChildMain.jsx
+++ src/components/ChildMain/ChildMain.jsx
@@ -2,18 +2,14 @@ import React, { useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import ChildDashboard from '../../pages/ChildDashboard';
 import { ChildContext } from '../../providers/ChildProvider';
-import Spinner from '../Spinner';
 import HeaderNav from '../HeaderNav';
 import './ChildMain.scss';
 
 const ChildMain = () => {
-	const { child, childTasks } = useContext(ChildContext);
+	const { child } = useContext(ChildContext);
 
 	if (!child.loggedInStatus) {
 		return <Redirect to="/login" />;
-	}
-	if (childTasks === undefined) {
-		return <Spinner />;
 	}
 	return (
 		<div className="ChildMain">
