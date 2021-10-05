b14c6264a885d1f2dcc4e22d33deaede0038fdd8
Christopher Jones <tkcwebdev@gmail.com>
2021-10-05 10:14:49 -0700

    notifications: Progress toward notifications. May have to setup backend server.

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/analytics';
 import 'firebase/compat/auth';
+import 'firebase/compat/messaging';
 
 firebase.initializeApp({
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY2,
@@ -17,6 +18,32 @@ firebase.initializeApp({
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 export const analytics = firebase.analytics();
+const messaging = firebase.messaging();
+
+// Request permission from user to allow push messages.
+// messaging.onMessage((payload) => {
+// 	console.log('Have permission', payload);
+// });
+
+messaging
+	.getToken({ vapidKey: process.env.REACT_APP_FIREBASE_MESSAGING_VAPID_KEY })
+	.then((currentToken) => {
+		if (currentToken) {
+			// Send the token to your server and update the UI if necessary
+			// ...
+			console.log(currentToken);
+		} else {
+			// Show permission request UI
+			console.log(
+				'No registration token available. Request permission to generate one.'
+			);
+			// ...
+		}
+	})
+	.catch((err) => {
+		console.log('An error occurred while retrieving token. ', err);
+		// ...
+	});
 
 // Firebase Emulators Configuration
 if (location.hostname === 'localhost') {
