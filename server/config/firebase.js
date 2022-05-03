021ebe7aadc11e8124bab6ba9ef95c39c7c55800
Christopher Jones <tkcwebdev@gmail.com>
2022-05-03 00:46:11 -0700

    77: Get service account file for firebase

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -3,10 +3,10 @@ const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 const { getAuth } = require('firebase-admin/auth');
 
-const serviceAccount = require('./ito2-a697c-3a5418499230.json');
+const serviceAccount = require('./ito2-a697c-e4741d895225.json');
 
 const app = initializeApp({
-    credential: cert(serviceAccount)
+	credential: cert(serviceAccount),
 });
 
 const auth = getAuth(app);
